6f0fa63e54ca708f62788c9cf968ff2d97f4deb2
Garvit729 <122528595+Garvit729@users.noreply.github.com>
2023-02-10 19:17:54 +0530

    Add files via upload

---
+++ cv.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+char ch;
+ch=cin.get();
+
+if(ch==' ' || '\n'){
+	cout<<"whitespaces";
+}
+
+else if (ch>='A' && ch<='Z' || ch>='a  && ch<='z'){
+
+	cout<<"letters";
+}
+
+else if (ch>='0'  && ch<='9'){
+	cout<<"Digits";
+
+}
+else{
+	cout<<"special symbols";
+}
+
+
+return 0;
+}
